105b31163615ab12d9e2011cf70eab0f581f11b3
NatoliChris <natolichris@gmail.com>
2017-01-17 10:06:04 +1100

    adding numbers to the blockchain, fixing up minor adjustments

---
+++ js/global.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	var count = 0;
+	setInterval(function(){
+		var a = $('.block');
+		for(var i = 0; i < a.length; ++i){
+			$(a[i]).text('#' + (count+i));
+		}
+		count++;
+	}, 1500);
+});
